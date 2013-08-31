73e3d66528b6b502a250084636f6cf38473f21d3
hATrayflood <h.rayflood@gmail.com>
2013-08-31 18:58:53 +0900

    version up to 0.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup;
 setup(
 	name="rfZenHan",
-	version="0.3.1",
+	version="0.4",
 	py_modules=["rfZenHan"],
 	scripts=['rfzenhan', 'rfzenhan.bat', 'rfZenHanCmd.py'],
 )
